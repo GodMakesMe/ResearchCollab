f0e85c7b89766ee6eb8fa5060d8a552dd331a35f
GodMakesMe <jaypeekrishna17@gmail.com>
2025-04-12 21:04:25 +0530

    funding addition of paging

--- backend-api/src/controllers/fundingController.js
+++ backend-api/src/controllers/fundingController.js
@@ -1,13 +1,31 @@
 const pool = require('../db/db');
 
 const getAllFunding = async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const offset = (page - 1) * limit;
+
   try {
-    const { rows } = await pool.query('SELECT * FROM funding');
-    res.status(200).json(rows);
+    const result = await pool.query('SELECT * FROM funding ORDER BY id LIMIT $1 OFFSET $2', [limit, offset]);
+    const totalCount = await pool.query('SELECT COUNT(*) FROM funding');
+
+    const totalPages = Math.ceil(totalCount.rows[0].count / limit);
+
+    res.status(200).json({
+      fundings: result.rows,
+      pagination: {
+        page,
+        totalPages,
+        totalCount: parseInt(totalCount.rows[0].count),
+      }
+    });
   } catch (err) {
+    console.error('Error fetching funding data:', err);
     res.status(500).send('Error fetching all funding');
   }
 };
+
+
 
 const addFunding = async (req, res) => {
   const { projectid, source, amount, utilization_status } = req.body;
