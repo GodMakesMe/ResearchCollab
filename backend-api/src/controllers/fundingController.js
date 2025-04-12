83367ea194c20f9a9b8f6e216d8d2ebf64ed7da0
GodMakesMe <jaypeekrishna17@gmail.com>
2025-04-12 22:29:08 +0530

    .smallfix

--- backend-api/src/controllers/fundingController.js
+++ backend-api/src/controllers/fundingController.js
@@ -1,12 +1,13 @@
 const pool = require('../db/db');
 
+
 const getAllFunding = async (req, res) => {
   const page = parseInt(req.query.page) || 1;
   const limit = parseInt(req.query.limit) || 10;
   const offset = (page - 1) * limit;
 
   try {
-    const result = await pool.query('SELECT * FROM funding ORDER BY id LIMIT $1 OFFSET $2', [limit, offset]);
+    const result = await pool.query('SELECT * FROM funding ORDER BY funding_id LIMIT $1 OFFSET $2', [limit, offset]);
     const totalCount = await pool.query('SELECT COUNT(*) FROM funding');
 
     const totalPages = Math.ceil(totalCount.rows[0].count / limit);
