9a0e942490697d53e363219be810507291670ef7
GodMakesMe <jaypeekrishna17@gmail.com>
2025-04-15 22:02:39 +0530

    filterPageAddition

--- backend-api/src/routes/projectsRoutes.js
+++ backend-api/src/routes/projectsRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { getAllProjects, addProjects, getProjectById, getProjects, partialUpdateProject, editProject, deleteProject } = require('../controllers/projectsController');
-const { authenticateAdmin } = require('../middleware/authMiddleware');
+const { getAllProjects, addProjects, getProjectById, getProjects, partialUpdateProject, editProject, deleteProject, filterProjectsMultiple, getTopDomains } = require('../controllers/projectsController');
+const { authenticateAdmin, authenticateUser } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 router.get('/all-projects', getAllProjects);
@@ -10,6 +10,8 @@ router.post('/', authenticateAdmin, addProjects);
 router.put('/:projectId', authenticateAdmin, editProject); // Assuming you want to edit project with a specific ID
 router.patch('/:projectId', authenticateAdmin, partialUpdateProject); // Assuming you want to partially edit project with a specific ID
 router.delete('/:projectId', authenticateAdmin, deleteProject); // Assuming you want to delete project with a specific ID
+router.get('/filter', authenticateUser, filterProjectsMultiple); // Assuming you want to filter projects based on multiple criteria)
+router.get('/top-domains', authenticateUser, getTopDomains); 
 
 
 
